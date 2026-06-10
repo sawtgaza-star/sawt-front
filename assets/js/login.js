83e6da03b54ebcaf18a12bdda181fcb9f2c216e6
Anas Ahmed Maliha <70707113+AnasMaliha@users.noreply.github.com>
2026-06-10 18:00:05 +0300

    add pages verification

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,25 @@
-// تحديد العناصر من الـ DOM
+// --- الحقل الأول (كلمة المرور الجديدة) ---
 const passwordInput = document.getElementById('password');
 const togglePasswordIcon = document.getElementById('togglePassword');
 
-// إضافة حدث عند الضغط على الأيقونة
 togglePasswordIcon.addEventListener('click', function () {
-    // التحقق من نوع الحقل الحالي
     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordInput.setAttribute('type', type);
     
-    // تغيير شكل الأيقونة (العين والعين المشطوبة)
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+});
+
+
+// --- الحقل الثاني (إعادة إدخال كلمة المرور) ---
+const setPasswordInput = document.getElementById('set_password');
+const setTogglePasswordIcon = document.getElementById('set_togglePassword');
+
+setTogglePasswordIcon.addEventListener('click', function () {
+    // هنا نتحقق من حقل التأكيد الثاني ونغيره
+    const type = setPasswordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    setPasswordInput.setAttribute('type', type);
+    
     this.classList.toggle('fa-eye');
     this.classList.toggle('fa-eye-slash');
 });
